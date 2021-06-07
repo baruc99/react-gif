60b553f2d6ba9c10ab90e578e76ef79c4e4deab6
Baruc Sanchez <barucgs@gmail.com>
2021-06-07 13:07:33 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+
+    const [categories, setcategories] = useState([ 'pokemon']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>  
+            <AddCategory setCategories={setcategories} />
+            <hr />
+            <ol>
+                {
+                    categories.map(category =>(
+                        <GifGrid
+                            key={category}
+                            category={category}  
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
